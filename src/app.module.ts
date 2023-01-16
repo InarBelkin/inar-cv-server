bbbd2ddb771604c3459b94b3bb3832a66f652695
Inar <Inar2000s@mail.ru>
2023-01-16 23:21:52 +0300

    CORS corrections

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { LoggerMiddleware } from './MiddleHuyna';
+import { LoggerMiddleware } from './LoggerMiddleware';
 import { RolesModule } from './roles/roles.module';
 import { MailModule } from './mail/mail.module';
 import { CommentsModule } from './comments/comments.module';
